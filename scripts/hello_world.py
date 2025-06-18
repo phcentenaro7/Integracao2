b61f274e3f2570ac238f095de539d4061002850e
Pedro Henrique Centenaro <phc7blu@gmail.com>
2025-06-18 17:55:45 -0300

    Teste de login e senha

--- scripts/hello_world.py
+++ scripts/hello_world.py
@@ -1,8 +1,15 @@
+from cgi import parse_qs
+
 def application(environ, start_response):
     status = '200 OK'
-    output = 'Hello, world!'
+    output = 'Olá, mundo!<br>'
+    content_length = environ.get('CONTENT_LENGTH', 0)
+    if content_length > 0:
+        fields = parse_qs(environ['wsgi.input'].read(content_length))
+        output += 'Seu login: ' + fields.get('login', '') + '<br>'
+        output += 'Sua senha: ' + fields.get('senha', '') + '<br>'
 
-    headers = [("Contenty-type", "text/plain")]
+    headers = [('Contenty-type', 'text/plain')]
 
     start_response(status, headers)
     return [output.encode()]
