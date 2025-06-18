176da4af04fa9c07ced752f2e2c0a91877061a88
Pedro Henrique Centenaro <phc7blu@gmail.com>
2025-06-18 20:17:22 -0300

    Changed app to application

--- scripts/hello_world.py
+++ scripts/hello_world.py
@@ -1,10 +1,10 @@
 from flask import Flask
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/hello_world')
+@application.route('/hello_world')
 def sayHello():
     return "Hello, world!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(debug=True)
