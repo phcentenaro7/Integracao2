cc5016a41155568a079c6200f68c8d02a50d3f0a
Pedro Henrique Centenaro <phc7blu@gmail.com>
2025-06-18 18:04:30 -0300

    Fixed bugs

--- scripts/hello_world.py
+++ scripts/hello_world.py
@@ -3,9 +3,9 @@ from urllib import parse
 def application(environ, start_response):
     status = '200 OK'
     output = 'Olá, mundo!<br>'
-    content_length = environ.get('CONTENT_LENGTH', 0)
+    content_length = int(environ.get('CONTENT_LENGTH', 0))
     if content_length > 0:
-        fields = parse_qs(environ['wsgi.input'].read(content_length))
+        fields = parse(environ['wsgi.input'].read(content_length))
         output += 'Seu login: ' + fields.get('login', '') + '<br>'
         output += 'Sua senha: ' + fields.get('senha', '') + '<br>'
 
