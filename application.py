13e7b1ea6612c2a4b3a94b6610b8d4bb4b682d57
Pedro Henrique Centenaro <phc7blu@gmail.com>
2025-06-18 22:34:24 -0300

    Testes de POST

--- application.py
+++ application.py
@@ -2,11 +2,11 @@ from flask import Flask, send_file, request, render_template
 
 application = Flask(__name__)
 
-@application.route('/', methods=['POST'])
+@application.route('/', methods=['GET', 'POST'])
 def serveMainPage():
     return render_template('index.html')
 
-@application.route('/hello_world', methods=['POST'])
+@application.route('/hello_world', methods=['GET', 'POST'])
 def sayHello():
     return 'Hello, world!\nLogin: ' + request.form.get('login') + '\nSenha: ' + request.form.get('senha')
 
