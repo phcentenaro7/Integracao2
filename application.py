a00d92d4eb7ba407d69f54277920c678273dcab6
Pedro Henrique Centenaro <phc7blu@gmail.com>
2025-06-18 23:46:05 -0300

    socorro

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-from flask import Flask, send_file, url_for, request, render_template
+from flask import Flask, send_file, url_for, request, render_template, redirect
 
 application = Flask(__name__)
 
