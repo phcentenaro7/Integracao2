1163a31b097d774ab9a81ff962e57513e6406b0d
Pedro Henrique Centenaro <phc7blu@gmail.com>
2025-06-18 21:00:58 -0300

    Reestruturação

--- application.py
+++ application.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, send_file
 
 application = Flask(__name__)
+
+@application.route('/')
+def serveMainPage():
+    return send_file('index.html')
 
 @application.route('/hello_world')
 def sayHello():
