dea30f68e0546878a042aef196d0950df6ed5922
Pedro Henrique Centenaro <phc7blu@gmail.com>
2025-06-19 17:45:25 -0300

    Redis test

--- application.py
+++ application.py
@@ -20,6 +20,7 @@ def verifyLogin():
                  "WHERE (username=%s OR email=%s) AND password=%s")
         cursor.execute(query, (login, login, password))
         if cursor.rowcount > 0:
+            activeUsersDB.setex(request.remote_addr, 60, (login, 0))
             cursor.close()
             return redirect(url_for('sayHello', login=login, password=password))
         cursor.close()
