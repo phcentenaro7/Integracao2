a150dea178c31355affc1fc27cb215dab36f1017
Pedro Henrique Centenaro <phc7blu@gmail.com>
2025-06-18 23:44:15 -0300

    teste POST

--- application.py
+++ application.py
@@ -2,14 +2,16 @@ from flask import Flask, send_file, url_for, request, render_template
 
 application = Flask(__name__)
 
-@application.route('/', methods=['GET', 'POST'])
+@application.route('/', methods=['GET'])
 def serveMainPage():
+    return render_template('index.html')
+
+@application.route('/verify', methods=['GET', 'POST'])
+def verifyLogin():
     if request.method == 'POST':
         login = request.form['login']
         password = request.form['senha']
         return redirect(url_for('sayHello', login=login, password=password))
-    else:
-        return render_template('index.html')
 
 @application.route('/hello_world/<login>/<password>', methods=['GET', 'POST'])
 def sayHello(login, password):
